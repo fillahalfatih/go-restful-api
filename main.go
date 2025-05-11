a599f431f970c0d54134c7a4c257095ef3aa9d79
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-05-11 09:37:49 +0700

    add DELETE book data

--- main.go
+++ main.go
@@ -58,12 +58,20 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 // Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	
 }
 
 // Delete a Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.Id == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
