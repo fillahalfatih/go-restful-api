62f8235a849279f0991b734147ac1e178206e8ba
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-05-11 09:23:05 +0700

    add GET single book

--- main.go
+++ main.go
@@ -33,7 +33,15 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 
 // Get Single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // Create a New Book
