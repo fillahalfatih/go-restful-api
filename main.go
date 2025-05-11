ae001e9131cc7595224a1522569dff2b0470400e
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-05-11 09:56:53 +0700

    add PUT book data

--- main.go
+++ main.go
@@ -58,7 +58,20 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 // Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.Id == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.Id = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Delete a Book
